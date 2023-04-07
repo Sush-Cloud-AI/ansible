3f3260c2e02fe6be76d386606a78eae7c22a4175
Sush <sushcloud12@gmail.com>
2023-04-07 10:13:51 +0100

    i

--- shell/server-create.sh
+++ shell/server-create.sh
@@ -2,10 +2,10 @@
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=devops" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
 COMPONENT=$1
-
+SGID="sg-03ec6d3f23f96aefd"
 
 SERVER_CREATE(){
-    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type t3.micro  --security-group-ids sg-03ec6d3f23f96aefd --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g') 
+    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type t3.micro  --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g') 
     echo $PRIVATE_IP
 
 
